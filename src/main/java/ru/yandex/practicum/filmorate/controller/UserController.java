a6de58c36fb6ebb10a045c2e327fd73beb78f997
fen1x63 <118061645+fen1x63@users.noreply.github.com>
2023-04-17 14:07:57 +0300

    Update UserController.java

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -23,23 +25,24 @@ public class UserController {
     @PostMapping
     public User createUser(@Valid @RequestBody User user) {
 
-        user.setId(++currentMaxId);
+        user.setId(currentMaxId);
         users.put(currentMaxId, user);
         log.info("createUser: {}", user);
         return user;
     }
 
     @PutMapping
-    public User updateUser(@Valid @RequestBody User user) {
+    public ResponseEntity<String> updateUser(@Valid @RequestBody User user) {
 
         if (users.containsKey(user.getId())) {
             users.put(user.getId(), user);
             log.info("updateUser: {}", user);
-            return user;
+            return ResponseEntity.status(HttpStatus.OK).body("Пользователь изменён");
         } else {
+
             log.info("error updateUser without ID: {}", user);
         }
-        return null;
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Пользователь не найден");
     }
 
     @GetMapping
