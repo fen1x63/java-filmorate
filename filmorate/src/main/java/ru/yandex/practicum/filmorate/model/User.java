ca1f254651d8b4a09c778346e9d50b40864a2d35
fen1x63 <118061645+fen1x63@users.noreply.github.com>
2023-07-19 14:49:50 +0300

    ТЗ 9 первый коммит

---
+++ filmorate/src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
+import java.time.LocalDate;
+
+
+
+@Data
+@Builder
+public class User {
+
+    private int id;
+    @Email
+    @NonNull
+    private String email;
+    @NonNull
+    @NotBlank
+    private String login;
+    private String name;
+    @Past
+    private LocalDate birthday;
+}
