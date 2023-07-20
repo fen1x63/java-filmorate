ae1ba2f8f995a6533785e5065b6d454f7444c7c3
fen1x63 <118061645+fen1x63@users.noreply.github.com>
2023-07-20 16:07:02 +0300

    Update User.java

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -1,7 +1,6 @@
 package ru.yandex.practicum.filmorate.model;
 
 import lombok.Builder;
-import lombok.Data;
 import lombok.NonNull;
 
 import javax.validation.constraints.Email;
@@ -10,8 +9,6 @@ import javax.validation.constraints.Past;
 import java.time.LocalDate;
 
 
-
-@Data
 @Builder
 public class User {
 
@@ -25,4 +22,44 @@ public class User {
     private String name;
     @Past
     private LocalDate birthday;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
 }
